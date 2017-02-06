2334a4998741ab89a8075244aaccdade01a247a7
yayoc <n.yayoshi+github@gmail.com>
2017-02-06 09:44:03 +0900

    Update component. Add validateOnBlur, onBlur, onFocus props.

--- src/components/ReactTextField.js
+++ src/components/ReactTextField.js
@@ -13,14 +13,35 @@ export default class ReactTextField extends React.Component {
       isValid: false,
     };
     this.onChange = this.onChange.bind(this);
+    this.onBlur = this.onBlur.bind(this);
+    this.onFocus = this.onFocus.bind(this);
   }
 
   onChange(e) {
-    const value = e.target.value;
-    this.validate(value);
+    if (!this.props.validateOnBlur) {
+      const value = e.target.value;
+      this.validate(value);
+    }
     // Execute callback function.
     if (this.props.onChange) {
       this.props.onChange(this.state.isValid);
+    }
+  }
+
+  onBlur(e) {
+    const value = e.target.value;
+    if (this.props.validateOnBlur) {
+      this.validate(value);
+    }
+
+    if (this.props.onBlur) {
+      this.props.onBlur(e);
+    }
+  }
+
+  onFocus(e) {
+    if (this.props.onFocus) {
+      this.prop.onFocus(e);
     }
   }
 
@@ -46,7 +67,14 @@ export default class ReactTextField extends React.Component {
   render() {
     return (
       <div className="ReactTextField--container">
-        <input type={this.props.type} className="ReactTextField--input" onChange={this.onChange} />
+        <input
+          type={this.props.type}
+          className="ReactTextField--input"
+          onChange={this.onChange}
+          onBlur={this.onBlur}
+          onFocus={this.onFocus}
+          placeholder={this.props.placeholder}
+        />
         { this.state.isValid ?
           <span className="ReactTextField-message ReactTextField--success">
             {this.props.successMessage}
@@ -71,6 +99,8 @@ ReactTextField.propTypes = {
     'url',
   ]),
 
+  placeholder: PropTypes.string,
+
   // Success message when validator passed.
   successMessage: PropTypes.string,
 
@@ -80,12 +110,18 @@ ReactTextField.propTypes = {
     validator: PropTypes.func.isRequired,
   })),
 
-  onChange: React.PropTypes.func,
+  onChange: PropTypes.func,
+  onBlur: PropTypes.func,
+  onFocus: PropTypes.func,
+
+  validateOnBlur: PropTypes.bool,
 
 };
 
 ReactTextField.defaultProps = {
   type: 'text',
+  placeholder: '',
   validators: [],
+  validateOnBlur: false,
 };
 
