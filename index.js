f6013c7555afa1c0a8039734f133d103d902d679
yayoc <n.yayoshi+github@gmail.com>
2017-02-07 17:48:17 +0900

    Add npm ignore.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-module.exports = require('./lib/ReactTextField.jsx').default;
+var ReactTextField = require('./lib/components/ReactTextField.jsx').default;
+var validator = require('./lib/utils/validator').default;
+
+module.exports = {
+  ReactTextField,
+  validator,
+};
 
