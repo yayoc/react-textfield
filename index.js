ae5db5efc94c78c65715e9676865d426d9f30906
yayoc <n.yayoshi+github@gmail.com>
2017-03-01 14:54:14 +0900

    Change index.js in es5.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+'use strict';
+
 var ReactTextField = require('./lib/components/ReactTextField').default;
 var validator = require('./lib/utils/validator');
 
 module.exports = {
-  ReactTextField,
-  validator,
+  ReactTextField: ReactTextField,
+  validator: validator
 };
-
